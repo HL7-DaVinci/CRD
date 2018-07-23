3019fa895f1dd4ef5375a291c0a8a2cc3def9752
Keeyan Ghoreshi <kghoreshi@mitre.org>
2018-07-23 17:31:25 -0400

    Updated application to initialize database from CSV

--- server/src/main/java/fhir/restful/Application.java
+++ server/src/main/java/fhir/restful/Application.java
@@ -1,9 +1,19 @@
 package fhir.restful;
 
 
+import fhir.restful.Database.DataRepository;
+import fhir.restful.Database.Datum;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 @SpringBootApplication
 // Finds the FhirServlet and runs it
@@ -19,5 +29,51 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public CommandLineRunner loadData(DataRepository repository) {
+        return (args) -> {
+            // save a couple of customers
+            readCSV(repository);
+            repository.save(new Datum("18","80","MF","219","94660","false"
+                    ,"https://www.cms.gov/Outreach-and-Education/Medicare-Learning-Network-MLN/MLNProducts/downloads/PAP_DocCvg_Factsheet_ICN905064.pdf"));
+            repository.save(new Datum("18","90","MF","002","97542","true",
+                    "https://www.cms.gov/Outreach-and-Education/Medicare-Learning-Network-MLN/MLNProducts/downloads/PMDFactSheet07_Quark19.pdf"
+            ));
+        };
+    }
+
+
+    public void readCSV(DataRepository repository){
+
+        String csvFile = "crd_table_basic.csv";
+        try {
+            File file = new ClassPathResource("crd_table_basic.csv").getFile();
+            csvFile = file.getCanonicalPath();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String line = "";
+        String cvsSplitBy = ",";
+
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            br.readLine(); // Remove header
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] c = line.split(cvsSplitBy);
+
+                repository.save(new Datum(c[0],c[1],c[2],c[3],c[4],c[5],c[6]));
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
 }
