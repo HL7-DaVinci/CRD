abeb2c46c860e761ed0e3fba5d45895df73ca1ae
Andy Gregorowicz <andy@mitre.org>
2018-08-20 11:09:52 -0400

    Adding JUnit based tests
    Tests for the CRD CDS service as well as JSON deserialization

---
+++ server/src/test/java/endpoint/cdshooks/services/crd/CrdCdsServiceTest.java
@@ -0,0 +1,26 @@
+package endpoint.cdshooks.services.crd;
+
+import org.hl7.davinci.CrdRequestCreator;
+import org.hl7.davinci.cdshooks.CdsResponse;
+import org.hl7.davinci.cdshooks.orderreview.CrdCdsRequest;
+import org.hl7.fhir.r4.model.Enumerations;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Calendar;
+
+public class CrdCdsServiceTest {
+  @Test
+  public void testHandleRequest() {
+    Calendar cal = Calendar.getInstance();
+    cal.set(1970, Calendar.JULY, 4);
+    CrdCdsRequest request = CrdRequestCreator.createRequest(Enumerations.AdministrativeGender.MALE, cal.getTime());
+    CrdCdsService service = new CrdCdsService();
+    CdsResponse response = service.handleRequest(request);
+    assertNotNull(response);
+    assertEquals(1, response.getCards().size());
+    assertEquals("empty card", response.getCards().get(0).getSummary());
+  }
+}
