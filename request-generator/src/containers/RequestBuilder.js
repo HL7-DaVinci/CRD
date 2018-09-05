14e7fd2a6aba9b4398e47db7c9dcc73df69f1beb
Andy Gregorowicz <andy@mitre.org>
2018-09-05 10:59:41 -0400

    Removing unused imports

--- request-generator/src/containers/RequestBuilder.js
+++ request-generator/src/containers/RequestBuilder.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import { bindActionCreators} from 'redux';
-import { fetchCrdResponse } from '../actions';
+
 import InputBox from '../components/InputBox';
 import Toggle from '../components/Toggle';
 import DisplayBox from '../components/DisplayBox';
