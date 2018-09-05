dec435a223a346ac4b297db25fa165b6331fed9b
Cole Springate-Combs <cspringatecombs@mitre.org>
2018-09-05 14:57:02 -0400

    improve prefetch token declarations

--- resources/src/main/java/org/hl7/davinci/cdshooks/CrdPrefetch.java
+++ resources/src/main/java/org/hl7/davinci/cdshooks/CrdPrefetch.java
@@ -34,62 +34,6 @@ public class CrdPrefetch {
   @JsonDeserialize(using = JacksonBundleDeserializer.class)
   private Bundle supplyRequestBundle;
 
-  public static final String deviceRequestBundleKey = "deviceRequestBundle";
-  public static final String deviceRequestBundleQuery =
-      "DeviceRequest?id={{context.orders.DeviceRequest.id}}"
-          + "&_include=DeviceRequest:patient"
-          + "&_include=DeviceRequest:performer"
-          + "&_include=DeviceRequest:requester"
-          + "&_include=DeviceRequest:device"
-          + "&_include=PractitionerRole:organization"
-          + "&_include=PractitionerRole:practitioner"
-          + "&_include=DeviceRequest:insurance:Coverage";
-
-  public static final String medicationRequestBundleKey = "medicationRequestBundle";
-  public static final String medicationRequestBundleQuery =
-      "MedicationRequest?id={{context.orders.MedicationRequest.id}}"
-        + "&_include=MedicationRequest:patient"
-        + "&_include=MedicationRequest:intended-dispenser"
-        + "&_include=MedicationRequest:requester:PractitionerRole"
-        + "&_include=MedicationRequest:medication"
-        + "&_include=PractitionerRole:organization"
-        + "&_include=PractitionerRole:practitioner"
-        + "&_include=MedicationRequest:insurance:Coverage";
-
-  public static final String nutritionOrderBundleKey = "nutritionOrderBundle";
-  public static final String nutritionOrderBundleQuery =
-      "NutritionOrder?id={{context.orders.NutritionOrder.id}}"
-        + "&_include=NutritionOrder:patient"
-        + "&_include=NutritionOrder:provider"
-        + "&_include=NutritionOrder:requester"
-        + "&_include=PractitionerRole:organization"
-        + "&_include=PractitionerRole:practitioner"
-        + "&_include=NutritionOrder:encounter"
-        + "&_include=Encounter:location"
-        + "&_include=NutritionOrder:insurance:Coverage";
-
-  public static final String serviceRequestBundleKey = "serviceRequestBundle";
-  public static final String serviceRequestBundleQuery =
-      "ServiceRequest?id={{context.orders.ServiceRequest.id}}"
-        + "&_include=ServiceRequest:patient"
-        + "&_include=ServiceRequest:performer"
-        + "&_include=ServiceRequest:requester"
-        + "&_include=PractitionerRole:organization"
-        + "&_include=PractitionerRole:practitioner"
-        + "&_include=ServiceRequest:insurance:Coverage";
-
-  public static final String supplyRequestBundleKey = "supplyRequestBundle";
-  public static final String supplyRequestBundleQuery =
-      "SupplyRequest?id={{context.orders.SupplyRequest.id}}&"
-        + "_include=SupplyRequest:patient"
-        + "&_include=SupplyRequest:supplier:Organization"
-        + "&_include=SupplyRequest:requester:Practitioner"
-        + "&_include=SupplyRequest:requester:Organization"
-        + "&_include=SupplyRequest:Requester:PractitionerRole"
-        + "&_include=PractitionerRole:organization"
-        + "&_include=PractitionerRole:practitioner"
-        + "&_include=SupplyRequest:insurance:Coverage";
-
   public Bundle getDeviceRequestBundle() {
     return deviceRequestBundle;
   }
