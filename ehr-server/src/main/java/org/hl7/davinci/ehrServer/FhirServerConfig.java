28773154b7566ae0603ae8fda97c8956b4131db8
Keeyan Ghoreshi <kghoreshi@mitre.org>
2018-09-19 16:39:48 -0400

    added security to the server, won't work without a client secret in fhirServer.properties

--- ehr-server/src/main/java/org/hl7/davinci/ehrServer/FhirServerConfig.java
+++ ehr-server/src/main/java/org/hl7/davinci/ehrServer/FhirServerConfig.java
@@ -1,5 +1,6 @@
 package org.hl7.davinci.ehrServer;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
@@ -7,6 +8,7 @@ import javax.sql.DataSource;
 
 import ca.uhn.fhir.jpa.search.LuceneSearchMappingFactory;
 import ca.uhn.fhir.jpa.util.DerbyTenSevenHapiFhirDialect;
+import ca.uhn.fhir.rest.server.interceptor.CorsInterceptor;
 import org.apache.commons.dbcp2.BasicDataSource;
 //import org.apache.commons.lang3.time.DateUtils;
 //import org.hibernate.jpa.HibernatePersistenceProvider;
@@ -25,6 +27,7 @@ import ca.uhn.fhir.jpa.util.SubscriptionsRequireManualActivationInterceptorR4;
 import ca.uhn.fhir.rest.server.interceptor.IServerInterceptor;
 import ca.uhn.fhir.rest.server.interceptor.LoggingInterceptor;
 import ca.uhn.fhir.rest.server.interceptor.ResponseHighlighterInterceptor;
+import org.springframework.web.cors.CorsConfiguration;
 
 /**
  * This is the primary configuration file for the example server
@@ -32,104 +35,130 @@ import ca.uhn.fhir.rest.server.interceptor.ResponseHighlighterInterceptor;
 @Configuration
 @EnableTransactionManagement()
 public class FhirServerConfig extends BaseJavaConfigR4 {
-	static final Logger logger = LoggerFactory.getLogger(FhirServerConfig.class);
+  static final Logger logger = LoggerFactory.getLogger(FhirServerConfig.class);
 
-	/**
-	 * Configure FHIR properties around the the JPA server via this bean
-	 */
-	@Bean()
-	public DaoConfig daoConfig() {
-		logger.info("FhirServerConfig::daoConfig()");
-		DaoConfig retVal = new DaoConfig();
-		retVal.setAllowMultipleDelete(true);
-		return retVal;
-	}
+  /**
+   * Configure FHIR properties around the the JPA server via this bean.
+   */
+  @Bean()
+  public DaoConfig daoConfig() {
+    logger.info("FhirServerConfig::daoConfig()");
+    DaoConfig retVal = new DaoConfig();
+    retVal.setAllowMultipleDelete(true);
+    return retVal;
+  }
 
-	/**
-	 * The following bean configures the database connection. The 'url' property value of "jdbc:derby:directory:jpaserver_derby_files;create=true" indicates that the server should save resources in a
-	 * directory called "jpaserver_derby_files".
-	 * 
-	 * A URL to a remote database could also be placed here, along with login credentials and other properties supported by BasicDataSource.
-	 */
-	@Bean(destroyMethod = "close")
-	public DataSource dataSource() {
-		logger.info("FhirServerConfig::dataSource()");
-		BasicDataSource retVal = new BasicDataSource();
-		retVal.setDriver(new org.apache.derby.jdbc.EmbeddedDriver());
-		retVal.setUrl("jdbc:derby:directory:target/jpaserver_derby_files;create=true");
-		retVal.setUsername("");
-		retVal.setPassword("");
-		return retVal;
-	}
+  /**
+   * The following bean configures the database connection. The 'url' property value of "jdbc:derby:directory:jpaserver_derby_files;create=true" indicates that the server should save resources in a
+   * directory called "jpaserver_derby_files".
+   * <p>
+   * A URL to a remote database could also be placed here, along with login credentials and other properties supported by BasicDataSource.
+   */
+  @Bean(destroyMethod = "close")
+  public DataSource dataSource() {
+    logger.info("FhirServerConfig::dataSource()");
+    BasicDataSource retVal = new BasicDataSource();
+    retVal.setDriver(new org.apache.derby.jdbc.EmbeddedDriver());
+    retVal.setUrl("jdbc:derby:directory:target/jpaserver_derby_files;create=true");
+    retVal.setUsername("");
+    retVal.setPassword("");
+    return retVal;
+  }
 
-	@Override
-	@Bean()
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-		logger.info("FhirServerConfig::entityManagerFactory()");
-		LocalContainerEntityManagerFactoryBean retVal = super.entityManagerFactory();
-		retVal.setPersistenceUnitName("HAPI_PU");
-		retVal.setDataSource(dataSource());
-		retVal.setJpaProperties(jpaProperties());
-		return retVal;
-	}
+  @Override
+  @Bean()
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    logger.info("FhirServerConfig::entityManagerFactory()");
+    LocalContainerEntityManagerFactoryBean retVal = super.entityManagerFactory();
+    retVal.setPersistenceUnitName("HAPI_PU");
+    retVal.setDataSource(dataSource());
+    retVal.setJpaProperties(jpaProperties());
+    return retVal;
+  }
 
-	private Properties jpaProperties() {
-		logger.info("FhirServerConfig::jpaProperties()");
-		Properties extraProperties = new Properties();
-		extraProperties.put("hibernate.dialect", DerbyTenSevenHapiFhirDialect.class.getName());
-		extraProperties.put("hibernate.format_sql", "true");
-		extraProperties.put("hibernate.show_sql", "false");
-		extraProperties.put("hibernate.hbm2ddl.auto", "update");
-		extraProperties.put("hibernate.jdbc.batch_size", "20");
-		extraProperties.put("hibernate.cache.use_query_cache", "false");
-		extraProperties.put("hibernate.cache.use_second_level_cache", "false");
-		extraProperties.put("hibernate.cache.use_structured_entries", "false");
-		extraProperties.put("hibernate.cache.use_minimal_puts", "false");
-		extraProperties.put("hibernate.search.model_mapping", LuceneSearchMappingFactory.class.getName());
-		extraProperties.put("hibernate.search.default.directory_provider", "filesystem");
-		extraProperties.put("hibernate.search.default.indexBase", "target/lucenefiles");
-		extraProperties.put("hibernate.search.lucene_version", "LUCENE_CURRENT");
+  private Properties jpaProperties() {
+    logger.info("FhirServerConfig::jpaProperties()");
+    Properties extraProperties = new Properties();
+    extraProperties.put("hibernate.dialect", DerbyTenSevenHapiFhirDialect.class.getName());
+    extraProperties.put("hibernate.format_sql", "true");
+    extraProperties.put("hibernate.show_sql", "false");
+    extraProperties.put("hibernate.hbm2ddl.auto", "update");
+    extraProperties.put("hibernate.jdbc.batch_size", "20");
+    extraProperties.put("hibernate.cache.use_query_cache", "false");
+    extraProperties.put("hibernate.cache.use_second_level_cache", "false");
+    extraProperties.put("hibernate.cache.use_structured_entries", "false");
+    extraProperties.put("hibernate.cache.use_minimal_puts", "false");
+    extraProperties.put("hibernate.search.model_mapping", LuceneSearchMappingFactory.class.getName());
+    extraProperties.put("hibernate.search.default.directory_provider", "filesystem");
+    extraProperties.put("hibernate.search.default.indexBase", "target/lucenefiles");
+    extraProperties.put("hibernate.search.lucene_version", "LUCENE_CURRENT");
 //		extraProperties.put("hibernate.search.default.worker.execution", "async");
-		return extraProperties;
-	}
+    return extraProperties;
+  }
 
-	/**
-	 * Do some fancy logging to create a nice access log that has details about each incoming request.
-	 */
-	public IServerInterceptor loggingInterceptor() {
-		logger.info("FhirServerConfig::loggingInterceptor()");
-		LoggingInterceptor retVal = new LoggingInterceptor();
-		retVal.setLoggerName("fhirtest.access");
-		retVal.setMessageFormat(
-				"Path[${servletPath}] Source[${requestHeader.x-forwarded-for}] Operation[${operationType} ${operationName} ${idOrResourceName}] UA[${requestHeader.user-agent}] Params[${requestParameters}] ResponseEncoding[${responseEncodingNoDefault}]");
-		retVal.setLogExceptions(true);
-		retVal.setErrorMessageFormat("ERROR - ${requestVerb} ${requestUrl}");
-		return retVal;
-	}
+  /**
+   * Do some fancy logging to create a nice access log that has details about each incoming request.
+   */
+  public IServerInterceptor loggingInterceptor() {
+    logger.info("FhirServerConfig::loggingInterceptor()");
+    LoggingInterceptor retVal = new LoggingInterceptor();
+    retVal.setLoggerName("fhirtest.access");
+    retVal.setMessageFormat(
+        "Path[${servletPath}] Source[${requestHeader.x-forwarded-for}] Operation[${operationType} ${operationName} ${idOrResourceName}] UA[${requestHeader.user-agent}] Params[${requestParameters}] ResponseEncoding[${responseEncodingNoDefault}]");
+    retVal.setLogExceptions(true);
+    retVal.setErrorMessageFormat("ERROR - ${requestVerb} ${requestUrl}");
+    return retVal;
+  }
 
-	/**
-	 * This interceptor adds some pretty syntax highlighting in responses when a browser is detected
-	 */
-	@Bean(autowire = Autowire.BY_TYPE)
-	public IServerInterceptor responseHighlighterInterceptor() {
-		logger.info("FhirServerConfig::responseHighlighterInterceptor()");
-		ResponseHighlighterInterceptor retVal = new ResponseHighlighterInterceptor();
-		return retVal;
-	}
+  /**
+   * This interceptor adds some pretty syntax highlighting in responses when a browser is detected
+   */
+  @Bean(autowire = Autowire.BY_TYPE)
+  public IServerInterceptor responseHighlighterInterceptor() {
+    logger.info("FhirServerConfig::responseHighlighterInterceptor()");
+    return new ResponseHighlighterInterceptor();
+  }
 
-	@Bean(autowire = Autowire.BY_TYPE)
-	public IServerInterceptor subscriptionSecurityInterceptor() {
-		logger.info("FhirServerConfig::subscriptionSecurityInterceptor()");
-		SubscriptionsRequireManualActivationInterceptorR4 retVal = new SubscriptionsRequireManualActivationInterceptorR4();
-		return retVal;
-	}
+  @Bean(autowire = Autowire.BY_TYPE)
+  public IServerInterceptor subscriptionSecurityInterceptor() {
+    logger.info("FhirServerConfig::subscriptionSecurityInterceptor()");
+    return new SubscriptionsRequireManualActivationInterceptorR4();
+  }
 
-	@Bean()
-	public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-		logger.info("FhirServerConfig::transactionManager()");
-		JpaTransactionManager retVal = new JpaTransactionManager();
-		retVal.setEntityManagerFactory(entityManagerFactory);
-		return retVal;
-	}
+
+  @Bean(autowire = Autowire.BY_TYPE)
+  public IServerInterceptor authorizationInterceptor() {
+    logger.info("FhirServerConfig::authorizationInterceptor");
+    return new ClientAuthorizationInterceptor();
+  }
+
+  @Bean(autowire = Autowire.BY_TYPE)
+  public IServerInterceptor corsInterceptor() {
+    logger.info("FhirServerConfig::corsInterceptor");
+    CorsConfiguration config = new CorsConfiguration();
+    config.addAllowedHeader("x-fhir-starter");
+    config.addAllowedHeader("Origin");
+    config.addAllowedHeader("Accept");
+    config.addAllowedHeader("X-Requested-With");
+    config.addAllowedHeader("Content-Type");
+
+    // Allow all origins for now since the port the server gets run on might change
+    config.addAllowedOrigin("*");
+
+    config.addExposedHeader("Location");
+    config.addExposedHeader("Content-Location");
+    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+
+    // Create the interceptor and register it
+    return new CorsInterceptor(config);
+  }
+
+  @Bean()
+  public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+    logger.info("FhirServerConfig::transactionManager()");
+    JpaTransactionManager retVal = new JpaTransactionManager();
+    retVal.setEntityManagerFactory(entityManagerFactory);
+    return retVal;
+  }
 
 }
