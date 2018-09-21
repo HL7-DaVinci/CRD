985ebe0cc5fe7bd4c9d220bc1441b420d1477842
Keeyan Ghoreshi <kghoreshi@mitre.org>
2018-09-21 15:11:44 -0400

    Updated requests to work with a generic context type and fhir version

--- resources/src/main/java/org/hl7/davinci/r4/crdhook/orderreview/OrderReviewRequest.java
+++ resources/src/main/java/org/hl7/davinci/r4/crdhook/orderreview/OrderReviewRequest.java
@@ -1,12 +1,14 @@
 package org.hl7.davinci.r4.crdhook.orderreview;
 
-import org.cdshooks.CdsRequest;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import java.util.HashMap;
+import org.cdshooks.CdsRequest;
 import org.hl7.davinci.r4.Utilities;
 import org.hl7.davinci.r4.crdhook.CrdPrefetch;
+import org.hl7.fhir.r4.model.Bundle;
 
-public class OrderReviewRequest extends CdsRequest<OrderReviewContext, CrdPrefetch> {
+import java.util.HashMap;
+
+public class OrderReviewRequest extends CdsRequest<CrdPrefetch, Bundle, OrderReviewContext> {
 
   private HashMap<String, Object> mapForPrefetchTemplates = null;
 
