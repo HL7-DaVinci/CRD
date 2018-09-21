a9a2bbd4adaba078cdee6e2ca290c334809a5321
Keeyan Ghoreshi <kghoreshi@mitre.org>
2018-09-21 15:14:35 -0400

    Moved request handling to top level abstract class, services now just pass up information the top level class needs

--- server/src/main/java/org/hl7/davinci/endpoint/cdshooks/services/crd/stu3/MedicationPrescribeService.java
+++ server/src/main/java/org/hl7/davinci/endpoint/cdshooks/services/crd/stu3/MedicationPrescribeService.java
@@ -1,34 +1,29 @@
 package org.hl7.davinci.endpoint.cdshooks.services.crd.stu3;
 
-import java.util.List;
-import javax.validation.Valid;
-import org.hl7.davinci.stu3.FhirComponents;
-import org.hl7.davinci.stu3.Utilities;
-import org.cdshooks.CdsResponse;
-import org.cdshooks.CdsService;
-import org.hl7.davinci.stu3.crdhook.CrdPrefetch;
-import org.hl7.davinci.stu3.crdhook.CrdPrefetchTemplateElements;
+import org.cdshooks.CdsRequest;
 import org.cdshooks.Hook;
 import org.cdshooks.Prefetch;
-import org.hl7.davinci.stu3.crdhook.medicationprescribe.MedicationPrescribeRequest;
-import org.hl7.davinci.endpoint.components.CardBuilder;
-import org.hl7.davinci.endpoint.components.PrefetchHydrator;
-import org.hl7.davinci.endpoint.database.CoverageRequirementRule;
+import org.hl7.davinci.endpoint.cdshooks.services.crd.CdsService;
 import org.hl7.davinci.endpoint.database.CoverageRequirementRuleFinder;
+import org.hl7.davinci.stu3.FhirComponents;
+import org.hl7.davinci.stu3.Utilities;
+import org.hl7.davinci.stu3.crdhook.CrdPrefetchTemplateElements;
+import org.hl7.davinci.stu3.crdhook.medicationprescribe.MedicationPrescribeRequest;
 import org.hl7.davinci.stu3.fhirresources.DaVinciMedicationRequest;
-import org.hl7.fhir.dstu3.model.MedicationRequest;
-import org.hl7.fhir.exceptions.FHIRException;
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.CodeableConcept;
 import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.exceptions.FHIRException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Component("stu3_MedicationPrescribeService")
-public class MedicationPrescribeService extends CdsService {
+public class MedicationPrescribeService extends
+    CdsService<Bundle, DaVinciMedicationRequest, Patient, CodeableConcept>  {
 
   public static final String ID = "medication-prescribe-crd";
   public static final String TITLE = "medication-prescribe Coverage Requirements Discovery";
@@ -37,7 +32,8 @@ public class MedicationPrescribeService extends CdsService {
       "Get information regarding the coverage requirements for durable medical equipment";
   public static final Prefetch PREFETCH;
   static final Logger logger = LoggerFactory.getLogger(MedicationPrescribeService.class);
-
+  static final String FHIRVERSION = "stu3";
+  static final FhirComponents FHIRCOMPONENTS = new FhirComponents();
   static {
     PREFETCH = new Prefetch();
     PREFETCH.put(CrdPrefetchTemplateElements.MEDICATION_REQUEST_BUNDLE.getKey(),
@@ -49,82 +45,22 @@ public class MedicationPrescribeService extends CdsService {
   CoverageRequirementRuleFinder ruleFinder;
 
   public MedicationPrescribeService() {
-    super(ID, HOOK, TITLE, DESCRIPTION, PREFETCH);
+    super(ID, HOOK, TITLE, DESCRIPTION, PREFETCH, FHIRCOMPONENTS,FHIRVERSION);
   }
 
-  /**
-   * Handle the post request to the service.
-   *
-   * @param request The json request, parsed.
-   */
-  public CdsResponse handleRequest(@Valid @RequestBody MedicationPrescribeRequest request) {
+  public CodeableConcept getCc(DaVinciMedicationRequest medicationRequest) throws FHIRException {
+    return medicationRequest.getMedicationCodeableConcept();
+  }
+  public Patient getPatient(DaVinciMedicationRequest medicationRequest) {
+    return (Patient) medicationRequest.getSubject().getResource();
+  }
 
-    logger.info("handleRequest: start");
-    logger.info(
-        "Medications bundle size: " + request.getContext().getMedications().getEntry().size());
-
-    FhirComponents fhirComponents = FhirComponents.getInstance();
-    if (request.getPrefetch() == null)
-      request.setPrefetch(new CrdPrefetch());
-    PrefetchHydrator prefetchHydrator = new PrefetchHydrator<Bundle>(this, request,
-        fhirComponents.getFhirContext());
-    prefetchHydrator.hydrate(); //prefetch is now as hydrated as possible
-
-    CdsResponse response = new CdsResponse();
-
-    Bundle medicationRequestBundle = request.getPrefetch().getMedicationRequestBundle();
-    if (medicationRequestBundle == null) {
-      logger.error("Prefetch medicationRequestBundle not a bundle");
-      response.addCard(CardBuilder.summaryCard(
-          "medicationRequestBundle could not be (pre)fetched in this request "));
-      return response;
-    }
-    List medicationRequestList = Utilities.getResourcesOfTypeFromBundle(
-        DaVinciMedicationRequest.class, medicationRequestBundle);
-
-    if (medicationRequestList.isEmpty()) {
-      logger.warn("Unable to find any profiled MedicationRequests, looking for standard ones.");
-      medicationRequestList = Utilities.getResourcesOfTypeFromBundle(
-          MedicationRequest.class, medicationRequestBundle);
-    }
-
-    for (Object mr : medicationRequestList) {
-      MedicationRequest medicationRequest = (MedicationRequest) mr;
-      Patient patient = null;
-      CodeableConcept cc = null;
-      try {
-        cc = medicationRequest.getMedicationCodeableConcept();
-      } catch (FHIRException fe) {
-        response
-            .addCard(CardBuilder.summaryCard("Unable to parse the medication code out of the request"));
-      }
-
-      // See if the patient is in the prefetch
-      try {
-        patient = (Patient) medicationRequest.getSubject().getResource();
-      } catch (Exception e) {
-        response
-            .addCard(CardBuilder.summaryCard("No patient could be (pre)fetched in this request"));
-      }
-
-      if (patient != null && cc != null) {
-        int patientAge = Utilities.calculateAge(patient);
-        List<CoverageRequirementRule> coverageRequirementRules = ruleFinder
-            .findRules(patientAge, patient.getGender(), cc.getCoding().get(0).getCode(),
-                cc.getCoding().get(0).getSystem());
-        if (coverageRequirementRules.size() == 0) {
-          response.addCard(CardBuilder.summaryCard("No documentation rules found"));
-        } else {
-          for (CoverageRequirementRule rule: coverageRequirementRules) {
-            response.addCard(CardBuilder.transform(rule));
-          }
-        }
-      }
-
-    }
-    CardBuilder.errorCardIfNonePresent(response);
-    logger.info("handleRequest: end");
-    return response;
+  @Override
+  public List<DaVinciMedicationRequest> getRequests(CdsRequest request) {
+    MedicationPrescribeRequest medicationPrescribeRequest = (MedicationPrescribeRequest) request;
+    Bundle medicationRequestBundle = medicationPrescribeRequest.getPrefetch().getMedicationRequestBundle();
+    Utilities util = new Utilities();
+    return util.getResourcesOfTypeFromBundle(DaVinciMedicationRequest.class, medicationRequestBundle);
   }
 }
 
