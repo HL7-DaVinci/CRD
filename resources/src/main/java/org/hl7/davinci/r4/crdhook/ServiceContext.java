661c27d702575692531e33c79c4588d47b49d484
Keeyan Ghoreshi <kghoreshi@mitre.org>
2018-09-21 15:06:14 -0400

    Added interfaces and abstract classes needed to abstract services

---
+++ resources/src/main/java/org/hl7/davinci/r4/crdhook/ServiceContext.java
@@ -0,0 +1,55 @@
+package org.hl7.davinci.r4.crdhook;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.hl7.davinci.ServiceContextT;
+import org.hl7.davinci.r4.JacksonBundleDeserializer;
+import org.hl7.davinci.r4.JacksonHapiSerializer;
+import org.hl7.fhir.instance.model.api.IBaseBundle;
+import org.hl7.fhir.r4.model.Bundle;
+
+import javax.validation.constraints.NotNull;
+
+public abstract class ServiceContext implements ServiceContextT<Bundle> {
+
+
+  /** The FHIR Patient.id of the current patient in context. REQUIRED */
+  @NotNull
+  private String patientId;
+
+  /** The FHIR Encounter.id of the current encounter in context. OPTIONAL */
+  private String encounterId;
+
+  /**
+   * STU3 - FHIR Bundle of MedicationRequest, ReferralRequest, ProcedureRequest, NutritionOrder,
+   * VisionPrescription. REQUIRED
+   */
+  @JsonSerialize(using = JacksonHapiSerializer.class)
+  @JsonDeserialize(using = JacksonBundleDeserializer.class)
+  private Bundle services;
+
+  public String getPatientId() {
+    return patientId;
+  }
+
+  public void setPatientId(String patientId) {
+    this.patientId = patientId;
+  }
+
+  public String getEncounterId() {
+    return encounterId;
+  }
+
+  public void setEncounterId(String encounterId) {
+    this.encounterId = encounterId;
+  }
+
+  public Bundle getServices() {
+    return services;
+  }
+
+  public void setServices(Bundle services) {
+    this.services = services;
+  }
+  
+}
