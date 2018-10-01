1687bc25e2b2f6b9a8d480420e7b53ec7e5d186a
Keeyan Ghoreshi <kghoreshi@mitre.org>
2018-10-01 17:18:07 -0400

    initialized repository

---
+++ server/src/main/request_logging/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import RequestLog from './containers/RequestLog';
+class App extends Component {
+  render() {
+    return (
+      <RequestLog />
+    );
+  }
+}
+
+export default App;
