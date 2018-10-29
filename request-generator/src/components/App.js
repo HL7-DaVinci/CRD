792a374acc9a8f31a7a7acf3fa111e0617e34a97
Keeyan Ghoreshi <kghoreshi@mitre.org>
2018-10-29 14:57:06 -0400

    added state columns to the data table

--- request-generator/src/components/App.js
+++ request-generator/src/components/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import RequestBuilder from '../containers/RequestBuilder';
 export default class App extends Component {
+    componentDidMount(){
+        document.title = "Request Generator"
+      }
     render() {
         return (
             <div>
