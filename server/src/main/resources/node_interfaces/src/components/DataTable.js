169e147489ffb68e22876041765231399b383fac
Jess Dempsey <jdempsey@mitre.org>
2018-11-26 08:27:58 -0500

    work in prog consolidation

--- server/src/main/resources/node_interfaces/src/components/DataTable.js
+++ server/src/main/resources/node_interfaces/src/components/DataTable.js
@@ -4,6 +4,7 @@ import Input from "../components/Input";
 import TextArea from "../components/TextArea";
 import Select from "../components/Select";
 import Button from "../components/Button";
+import Form from "../components/Form";
 
 import './index.css';
 export default class DataTable extends Component {
