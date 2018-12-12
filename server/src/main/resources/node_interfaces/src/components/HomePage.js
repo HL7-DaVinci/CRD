5a2768db4d4dd75e1863de1b81a8e2a7f1a1dc4a
Keeyan Ghoreshi <kghoreshi@mitre.org>
2018-12-12 16:15:31 -0500

    made small changes to the UI

--- server/src/main/resources/node_interfaces/src/components/HomePage.js
+++ server/src/main/resources/node_interfaces/src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import DataTable from './DataTable';
+import './HomePage.css';
 export default class HomePage extends Component {
     constructor(props){
         super(props);
@@ -14,11 +15,11 @@ export default class HomePage extends Component {
      render() {
          return (
              <div >
-                     <h1 className="header" id="henlo">Coverage Requirements Discovery Reference Implementation</h1>
+                     <h1 className="header" id="henlo">Coverage Requirements Discovery (CRD) Reference Implementation (RI)</h1>
     <p>
         Welcome to the reference implementation of the Coverage Requirements Discovery (CRD) service. This service operates following the implementation guide published
         at H7 and developed by the <a href="http://www.hl7.org/about/davinci/index.cfm?ref=common">Da Vinci Project</a>.
-        It is implemented as a <a href="https://cds-hooks.org/">CDS Hook</a>.
+        It is implemented with <a href="https://cds-hooks.org/" target="#">CDS Hooks</a>.
     </p>
     <p>
         This service returns responses to CRD queries based on a small database of examples it maintains.
