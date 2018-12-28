127bd7ee322b5846a0669ad7d00e40852835cb79
Andy Gregorowicz <andy@mitre.org>
2018-12-28 11:13:41 -0500

    Fixing broken import and rebuilding JS
    It appears that HomePage.css is missing and will need to be added later.

--- server/src/main/resources/node_interfaces/src/components/HomePage.js
+++ server/src/main/resources/node_interfaces/src/components/HomePage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import DataTable from './DataTable';
-import './HomePage.css';
+//import './HomePage.css';
 export default class HomePage extends Component {
     constructor(props){
         super(props);
