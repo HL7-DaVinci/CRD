8a2b1188429b5f4fbb0374470aa26a536b8608b3
Cole Springate-Combs <cspringatecombs@mitre.org>
2019-01-14 16:27:22 -0500

    add card builder test

--- server/src/test/java/org/hl7/davinci/endpoint/cdshooks/components/CardBuilderTest.java
+++ server/src/test/java/org/hl7/davinci/endpoint/cdshooks/components/CardBuilderTest.java
@@ -1,39 +1,25 @@
-//package org.hl7.davinci.endpoint.cdshooks.components;
-//
-//    import static org.junit.jupiter.api.Assertions.assertEquals;
-//    import static org.junit.jupiter.api.Assertions.assertNull;
-//
-//    import org.hl7.davinci.endpoint.components.CardBuilder;
-//    import org.hl7.davinci.endpoint.database.CoverageRequirementRule;
-//    import org.cdshooks.Card;
-//    import org.junit.jupiter.api.Test;
-//
-//public class CardBuilderTest {
-//  @Test
-//  public void testRulesWithNoAuthNeeded() {
-//    CoverageRequirementRule crr = new CoverageRequirementRule();
-//    crr.setAgeRangeHigh(80);
-//    crr.setAgeRangeLow(55);
-//    crr.setEquipmentCode("E0424");
-//    crr.setGenderCode("F".charAt(0));
-//    crr.setNoAuthNeeded(true);
-//    Card card = CardBuilder.transform(crr,"dummyLaunchUrl.com");
-//    assertEquals("No documentation is required for a device or service with code: E0424", card.getSummary());
-//    assertNull(card.getLinks());
-//  }
-//
-//  @Test
-//  public void testRulesWithAuthNeeded() {
-//    CoverageRequirementRule crr = new CoverageRequirementRule();
-//    crr.setAgeRangeHigh(80);
-//    crr.setAgeRangeLow(55);
-//    crr.setEquipmentCode("E0424");
-//    crr.setGenderCode("F".charAt(0));
-//    crr.setNoAuthNeeded(false);
-//    crr.setInfoLink("http://www.mitre.org");
-//    Card card = CardBuilder.transform(crr,"dummyLaunchUrl.com");
-//    assertEquals("Documentation is required for the desired device or service", card.getSummary());
-//    assertEquals(2, card.getLinks().size());
-//    assertEquals("http://www.mitre.org", card.getLinks().get(0).getUrl());
-//  }
-//}
+package org.hl7.davinci.endpoint.cdshooks.components;
+
+    import static org.junit.jupiter.api.Assertions.assertEquals;
+    import static org.junit.jupiter.api.Assertions.assertNull;
+
+    import org.hl7.davinci.endpoint.components.CardBuilder;
+    import org.hl7.davinci.endpoint.components.CardBuilder.CqlResultsForCard;
+    import org.cdshooks.Card;
+    import org.junit.jupiter.api.Test;
+
+public class CardBuilderTest {
+  @Test
+  public void testRulesWithNoAuthNeeded() {
+    CqlResultsForCard cardResults = new CardBuilder.CqlResultsForCard();
+    cardResults.setRuleApplies(true);
+    cardResults.setDetails("Some details.");
+    cardResults.setInfoLink("http://some.link");
+    cardResults.setSummary("The summary!");
+    Card card = CardBuilder.transform(cardResults,"launch_url");
+    assertEquals("The summary!", card.getSummary());
+    assertEquals("Some details.", card.getDetail());
+    assertEquals("http://some.link", card.getLinks().get(0).getUrl());
+    assertEquals("launch_url", card.getLinks().get(1).getUrl());
+  }
+}
