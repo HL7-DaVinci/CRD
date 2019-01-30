4efaa47c19d5618225082251b0dfc6497732fd5f
Keeyan Ghoreshi <kghoreshi@mitre.org>
2019-01-30 15:05:46 -0500

    minor fixes

--- server/src/main/resources/node_interfaces/src/containers/RequestLog.js
+++ server/src/main/resources/node_interfaces/src/containers/RequestLog.js
@@ -33,7 +33,7 @@ export default class RequestLog extends Component {
     }
 
     async getData(){
-        const requestData = await fetch('http://localhost:8090/api/requests', {
+        const requestData = await fetch(baseUrl+'api/requests', {
             method: 'GET',
             headers: {
                 'Accept': 'application/json'
