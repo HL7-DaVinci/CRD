b2daa4eec8c10f3ca836e7dad63ea062aafb9d74
Patrick LaRocque <plarocque@mitre.org>
2019-04-08 11:50:51 -0400

    Support to get any file from CDS Connect and a wrapper for fetchFhirUri to call into the local version for now.

---
+++ server/src/main/java/org/hl7/davinci/endpoint/cdsconnect/CdsConnectFhirUriFetcher.java
@@ -0,0 +1,38 @@
+package org.hl7.davinci.endpoint.cdsconnect;
+
+
+import org.hl7.davinci.endpoint.Application;
+import org.hl7.davinci.endpoint.YamlConfig;
+import org.hl7.davinci.endpoint.components.FhirUriFetcher;
+import org.hl7.davinci.endpoint.components.FhirUriFetcherLocal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+@Component
+@Profile("cdsConnect")
+public class CdsConnectFhirUriFetcher implements FhirUriFetcher {
+
+  private static Logger logger = Logger.getLogger(Application.class.getName());
+
+
+  YamlConfig config;
+
+  //@Autowired
+  FhirUriFetcherLocal localFetcher;
+
+  @Autowired
+  public CdsConnectFhirUriFetcher(YamlConfig yamlConfig) {
+    config = yamlConfig;
+    localFetcher = new FhirUriFetcherLocal(yamlConfig);
+  }
+
+  public Resource fetch(String fhirUri){
+    logger.info("CDS Connect FHIR URI Fetching not yet implemented, using local fetcher");
+    return localFetcher.fetch(fhirUri);
+  }
+}
