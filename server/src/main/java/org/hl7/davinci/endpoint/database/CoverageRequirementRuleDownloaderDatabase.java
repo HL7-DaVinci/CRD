6a5f6b9f8fdeb1e47796d520afa5e48c7d58544e
Patrick LaRocque <41444457+plarocque4@users.noreply.github.com>
2019-06-04 17:11:34 -0400

    [304] Convert CQL to ELM in CRD when downloading files for DTR (#135)

--- server/src/main/java/org/hl7/davinci/endpoint/database/CoverageRequirementRuleDownloaderDatabase.java
+++ server/src/main/java/org/hl7/davinci/endpoint/database/CoverageRequirementRuleDownloaderDatabase.java
@@ -1,16 +1,21 @@
 package org.hl7.davinci.endpoint.database;
 
+import org.apache.commons.io.FilenameUtils;
 import org.hl7.davinci.endpoint.YamlConfig;
+import org.hl7.davinci.endpoint.cql.CqlExecutionContextBuilder;
 import org.hl7.davinci.endpoint.cql.bundle.CqlBundleFile;
 import org.hl7.davinci.endpoint.rules.CoverageRequirementRuleDownloader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -36,14 +41,30 @@ public class CoverageRequirementRuleDownloaderDatabase implements CoverageRequir
   public CqlBundleFile getFile(String payer, String codeSystem, String code, String name) {
     CqlBundleFile bundleFile = null;
     // ignore the payer/codesystem/code
-    File file = Paths.get(config.getLocalDbFhirArtifacts(), name).toFile();
+    Path path = Paths.get(config.getLocalDbFhirArtifacts(), name);
 
-    try {
-      bundleFile = new CqlBundleFile();
-      bundleFile.setResource(new InputStreamResource(new FileInputStream(file))).setFilename(name);
-    } catch (FileNotFoundException e) {
-      logger.info("file not found: " + name);
-      bundleFile = null;
+    if (FilenameUtils.getExtension(name).toUpperCase().equals("CQL")) {
+      logger.info("Converting CQL to ELM");
+      try {
+        String cql = new String(Files.readAllBytes(path));
+        String elm = CqlExecutionContextBuilder.translateToElm(cql);
+        byte[] elmData = elm.getBytes();
+
+        bundleFile = new CqlBundleFile();
+        bundleFile.setFilename(name).setResource(new ByteArrayResource(elmData));
+
+      } catch (Exception e) {
+        logger.info("Error: could not convert CQL: " + e.getMessage());
+        return bundleFile;
+      }
+    } else {
+      try {
+        bundleFile = new CqlBundleFile();
+        bundleFile.setResource(new InputStreamResource(new FileInputStream(path.toFile()))).setFilename(name);
+      } catch (FileNotFoundException e) {
+        logger.info("file not found: " + name);
+        bundleFile = null;
+      }
     }
 
     return bundleFile;
