f4b8d14465ef1ff0ccf70c8375e1bb35dbbaa264
Patrick LaRocque <plarocque@mitre.org>
2020-01-15 09:37:33 -0500

    [434] Add order-sign hook to CRD - Connectathon

---
+++ resources/src/main/java/org/hl7/davinci/stu3/crdhook/ordersign/OrderSignRequest.java
@@ -0,0 +1,37 @@
+package org.hl7.davinci.stu3.crdhook.ordersign;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.cdshooks.CdsRequest;
+import org.hl7.davinci.stu3.Utilities;
+import org.hl7.davinci.stu3.crdhook.CrdPrefetch;
+
+import java.util.HashMap;
+
+public class OrderSignRequest extends CdsRequest<CrdPrefetch, OrderSignContext> {
+
+  private HashMap<String, Object> mapForPrefetchTemplates = null;
+
+  /**
+   * Gets the data from the context to put into the prefetch token.
+   * @return a map of prefetch attributes to their values
+   */
+  @JsonIgnore
+  public Object getDataForPrefetchToken() {
+    if (mapForPrefetchTemplates != null) {
+      return mapForPrefetchTemplates;
+    }
+    mapForPrefetchTemplates = new HashMap<>();
+    mapForPrefetchTemplates.put("user", this.getUser());
+
+    HashMap<String, Object> contextMap = new HashMap<>();
+    contextMap.put("patientId", getContext().getPatientId());
+    contextMap.put("encounterId", getContext().getEncounterId());
+    contextMap.put("draftOrders", Utilities.bundleAsHashmap(getContext().getDraftOrders()));
+    mapForPrefetchTemplates.put("context", contextMap);
+
+    return mapForPrefetchTemplates;
+  }
+
+
+}
+
