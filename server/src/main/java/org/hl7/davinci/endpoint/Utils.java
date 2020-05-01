e7bfb22ed8e8ad3e1b45a45b5e0077d21462cbce
Patrick LaRocque <plarocque@mitre.org>
2020-05-01 16:04:16 -0400

    more tests for url

--- server/src/main/java/org/hl7/davinci/endpoint/Utils.java
+++ server/src/main/java/org/hl7/davinci/endpoint/Utils.java
@@ -23,12 +23,27 @@ public class Utils {
 
   public static URL getApplicationBaseUrlTest(HttpServletRequest request) {
     try {
-      URL url = new URL(request.getScheme(), request.getServerName(), request.getServerPort(), request.getContextPath());
 
-      logger.info("test: scheme   : " + request.getHeader(HttpHeaders.X_FORWARDED_PROTO) + ", " + request.getScheme());
-      logger.info("test: host     : " + request.getHeader(HttpHeaders.HOST) + ", " + request.getServerName() + ", " + request.getHeader(HttpHeaders.X_FORWARDED_HOST));
+      String serverName = ((request.getHeader(HttpHeaders.X_FORWARDED_HOST) != null) ? request.getHeader(HttpHeaders.X_FORWARDED_HOST) : request.getServerName());
+      String scheme = ((request.getHeader(HttpHeaders.X_FORWARDED_PROTO) != null) ? request.getHeader(HttpHeaders.X_FORWARDED_PROTO) : request.getScheme());
+      int port = request.getServerPort();
+      URL url = null;
+      if (port != 80) {
+        url = new URL(scheme, serverName, port, request.getContextPath());
+      } else {
+        url = new URL(scheme, serverName, request.getContextPath());
+      }
+      URL oldUrl = new URL(request.getScheme(), request.getServerName(), request.getServerPort(), request.getContextPath());
+      //URL url = new URL(scheme, serverName, request.getContextPath());
+
+      logger.info("test: scheme   : " + request.getHeader(HttpHeaders.X_FORWARDED_PROTO) + ", " + request.getScheme() + " --> " + scheme);
+      logger.info("test: host     : " + request.getHeader(HttpHeaders.HOST) + ", " + request.getServerName() + ", " + request.getHeader(HttpHeaders.X_FORWARDED_HOST) + " --> " + serverName);
       logger.info("test: port     : " + request.getServerPort());
       logger.info("test: forwarded: " + request.getHeader(HttpHeaders.FORWARDED));
+      logger.info("test: context p: " + request.getContextPath());
+      logger.info("test: ---------------------------------------");
+      logger.info("test: Old URL  : " + oldUrl.toString());
+      logger.info("test: URL      : " + url.toString());
 
       return url;
     } catch (MalformedURLException e) {
