42ac858b2c87f4bfc7fd23b9d32a1c17067bb62b
Patrick LaRocque <plarocque@mitre.org>
2020-05-06 13:47:38 -0400

    Updates to URL building.

--- server/src/main/java/org/hl7/davinci/endpoint/Utils.java
+++ server/src/main/java/org/hl7/davinci/endpoint/Utils.java
@@ -9,21 +9,24 @@ public class Utils {
 
   public static URL getApplicationBaseUrl(HttpServletRequest request) {
     try {
-      // grab the forwarded values if they are not null
-      String serverName = ((request.getHeader(HttpHeaders.X_FORWARDED_HOST) != null) ? request.getHeader(HttpHeaders.X_FORWARDED_HOST) : request.getServerName());
-      String scheme = ((request.getHeader(HttpHeaders.X_FORWARDED_PROTO) != null) ? request.getHeader(HttpHeaders.X_FORWARDED_PROTO) : request.getScheme());
-      int port = ((request.getHeader(HttpHeaders.X_FORWARDED_PORT) != null) ? Integer.parseInt(request.getHeader(HttpHeaders.X_FORWARDED_PORT)) : request.getServerPort());
       URL url = null;
 
-      // grab the last forwarded url
-      String[] serverParts = serverName.split(", ");
-      serverName = serverParts[serverParts.length - 1];
+      // grab the forwarded values if they are not null
+      if (request.getHeader(HttpHeaders.X_FORWARDED_HOST) != null) {
+        String serverName = request.getHeader(HttpHeaders.X_FORWARDED_HOST);
 
-      // set the port if not the default http port (80)
-      if (port != 80) {
-        url = new URL(scheme, serverName, port, request.getContextPath());
+        // grab the last forwarded url
+        String[] serverParts = serverName.split(", ");
+        serverName = serverParts[serverParts.length - 1];
+
+        if (request.getHeader(HttpHeaders.X_FORWARDED_PORT) != null) {
+          url = new URL(request.getHeader(HttpHeaders.X_FORWARDED_PROTO), serverName, Integer.parseInt(request.getHeader(HttpHeaders.X_FORWARDED_PORT)), request.getContextPath());
+        } else {
+          url = new URL(request.getHeader(HttpHeaders.X_FORWARDED_PROTO), serverName, request.getContextPath());
+        }
+
       } else {
-        url = new URL(scheme, serverName, request.getContextPath());
+        url = new URL(request.getScheme(), request.getServerName(), request.getServerPort(), request.getContextPath());
       }
 
       return url;
