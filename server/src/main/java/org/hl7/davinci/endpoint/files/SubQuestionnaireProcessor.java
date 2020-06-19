f6c9a2e6b756a4c82714932bfb48c13d37d83720
Chris Hossenlopp <hossenlopp@mitre.org>
2020-06-19 10:14:14 -0400

    Added comments and made some naming changes.

--- server/src/main/java/org/hl7/davinci/endpoint/files/SubQuestionnaireProcessor.java
+++ server/src/main/java/org/hl7/davinci/endpoint/files/SubQuestionnaireProcessor.java
@@ -15,16 +15,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.hl7.fhir.r4.model.Resource;
 
+/**
+ * Processes FHIR R4 questionnaire to assemble sub-questionnaires into a complete questionnaire.
+ */
 public class SubQuestionnaireProcessor extends FhirResourceProcessor<Questionnaire> {
 
   static final Logger logger = LoggerFactory.getLogger(SubQuestionnaireProcessor.class);
 
+  /**
+   * Processes a Questionnaire to replace all sub-questionnaire references with the items.
+   */
 	@Override
 	protected Questionnaire processResource(Questionnaire inputResource, FileStore fileStore, String baseUrl) {
 		return this.assembleQuestionnaire(inputResource, fileStore, baseUrl);
 	}
   
-
+  /**
+   * Assemble a questionnaire to have all sub-questionnaires pulled into this questionnaire.
+   * 
+   * @param q The Questionnaire to assemble.
+   * @param fileStore The FileStore to be used for fetching sub-questionnaires.
+   * @param baseUrl The base url from the server.
+   * @return The assembled questionnaire.
+   */
   protected Questionnaire assembleQuestionnaire(Questionnaire q, FileStore fileStore, String baseUrl) {
     logger.info("SubQuestionnaireProcessor::assembleQuestionnaire(): " + q.getId());
 
@@ -38,7 +51,7 @@ public class SubQuestionnaireProcessor extends FhirResourceProcessor<Questionnai
 
     int containedSize = containedList.size();
 
-    parseItemList(q.getItem(), fileStore, baseUrl, containedList, extensionList);
+    processItemList(q.getItem(), fileStore, baseUrl, containedList, extensionList);
     
     if (containedSize != containedList.size())
       q.setContained(new ArrayList<org.hl7.fhir.r4.model.Resource>(containedList.values()));
@@ -46,14 +59,23 @@ public class SubQuestionnaireProcessor extends FhirResourceProcessor<Questionnai
     return q;
   }
 
-  private void parseItemList(List<QuestionnaireItemComponent> itemList, FileStore fileStore, String baseUrl,
+  /**
+   * Iterate over and modify a list of questionnaire items. This will up date the list of items as sub-questionnaire references are found.
+   * 
+   * @param itemList The Item list to iterate over.
+   * @param fileStore The FileStore to be used for fetching sub-questionnaires.
+   * @param baseUrl The base url from the server.
+   * @param containedList List of contained resources to put in the assembled Questionnaire. This will be filled while iterating.
+   * @param extensionList List of extensions to put in the assembled Questionnaire. This will be filled while iterating.
+   */
+  private void processItemList(List<QuestionnaireItemComponent> itemList, FileStore fileStore, String baseUrl,
     Hashtable<String, org.hl7.fhir.r4.model.Resource> containedList, List<Extension> extensionList) {
     if (itemList == null || itemList.size() == 0)
       return;
 
     for (int i = 0; i < itemList.size();) {
       List<QuestionnaireItemComponent> returnedItemList = 
-        parseItem(itemList.get(i), fileStore, baseUrl, containedList, extensionList);
+        processItem(itemList.get(i), fileStore, baseUrl, containedList, extensionList);
       
       if (returnedItemList.size() == 0) {
         continue;
@@ -71,7 +93,18 @@ public class SubQuestionnaireProcessor extends FhirResourceProcessor<Questionnai
     }
   }
 
-  private List<QuestionnaireItemComponent> parseItem(QuestionnaireItemComponent item, FileStore fileStore, String baseUrl,
+  /**
+   * Determines if this item is a sub-questionnaire reference and returns the items to replace it with. Returns the same list
+   * otherwise. Also recursively continues scanning if this is just a grouping item.
+   * 
+   * @param item The item to check for sub-questionnaire referece.
+   * @param fileStore The FileStore to be used for fetching sub-questionnaires.
+   * @param baseUrl The base url from the server.
+   * @param containedList List of contained resources to put in the assembled Questionnaire. This will be filled while iterating.
+   * @param extensionList List of extensions to put in the assembled Questionnaire. This will be filled while iterating.
+   * @return New list of items to replace this item with.
+   */
+  private List<QuestionnaireItemComponent> processItem(QuestionnaireItemComponent item, FileStore fileStore, String baseUrl,
   Hashtable<String, org.hl7.fhir.r4.model.Resource> containedList, List<Extension> extensionList) {
     // find if item has an extension is sub-questionnaire
     Extension e = item.getExtensionByUrl("http://hl7.org/fhir/StructureDefinition/sub-questionnaire");
@@ -106,7 +139,7 @@ public class SubQuestionnaireProcessor extends FhirResourceProcessor<Questionnai
     }
     
     // parse sub-items
-    this.parseItemList(item.getItem(), fileStore, baseUrl, containedList, extensionList);
+    this.processItemList(item.getItem(), fileStore, baseUrl, containedList, extensionList);
 
     return Arrays.asList(item);
   }
