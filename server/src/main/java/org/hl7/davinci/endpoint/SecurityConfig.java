e37a64efe9fb2a80af958d34fe1fc02476f0b14d
Patrick LaRocque <plarocque@mitre.org>
2021-05-14 13:39:11 -0400

    Try to fix CORS reload issue

--- server/src/main/java/org/hl7/davinci/endpoint/SecurityConfig.java
+++ server/src/main/java/org/hl7/davinci/endpoint/SecurityConfig.java
@@ -36,7 +36,7 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     final CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(ImmutableList.of("http://localhost:8080", "http://localhost:3000", "http://localhost:3002"));
+    configuration.setAllowedOrigins(ImmutableList.of("http://localhost:8080", "http://localhost:3000", "http://localhost:3002", "https://localhost:443"));
     configuration.setAllowedMethods(ImmutableList.of("HEAD",
         "GET", "POST", "PUT", "DELETE", "PATCH"));
     // setAllowCredentials(true) is important, otherwise:
