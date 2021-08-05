09922b88e3ece2441528e567a98945617a594e5a
lpqdao <lpqdao@gmail.com>
2021-08-05 19:08:18 -0400

    adding more

--- server/src/main/java/org/hl7/davinci/endpoint/cdshooks/services/crd/CdsService.java
+++ server/src/main/java/org/hl7/davinci/endpoint/cdshooks/services/crd/CdsService.java
@@ -148,6 +148,7 @@ public abstract class CdsService<requestTypeT extends CdsRequest<?, ?>> {
     } catch (RequestIncompleteException e) {
       logger.warn(e.getMessage() + "; summary card sent to client");
       response.addCard(CardBuilder.summaryCard(e.getMessage()));
+      requestLog.setCardListFromCards(response.getCards());
       requestLog.setResults(e.getMessage());
       requestService.edit(requestLog);
       return response;
@@ -210,9 +211,14 @@ public abstract class CdsService<requestTypeT extends CdsRequest<?, ?>> {
         logger.warn(msg + "; summary card sent to client");
         response.addCard(CardBuilder.summaryCard(msg));
       }
-
-      CardBuilder.errorCardIfNonePresent(response);
     }
+
+    CardBuilder.errorCardIfNonePresent(response);
+
+    // Ading card to requestLog
+    requestLog.setCardListFromCards(response.getCards());
+    requestService.edit(requestLog);
+
 
     return response;
   }
