fc7a03867d2a9ed41f20ebb3e3f97768c0a67461
Sahil Malhotra <Sahil Malhotra>
2021-12-03 12:56:02 -0500

    production docker and log/db location changes

--- dockerRunnerDev.sh
+++ dockerRunnerDev.sh
@@ -3,23 +3,24 @@
 # Handle closing application on signal interrupt (ctrl + c)
 trap 'kill $CONTINUOUS_BUILD_PID $SERVER_PID; gradle --stop; exit' INT
 
+mkdir logs 
 # Reset log file content for new application boot
-echo "*** Logs for 'gradle installBootDist --continuous' ***" > builder.log
-echo "*** Logs for 'gradle bootRun' ***" > runner.log
+echo "*** Logs for 'gradle installBootDist --continuous' ***" > ./logs/builder.log
+echo "*** Logs for 'gradle bootRun' ***" > ./logs/runner.log
 
 # Print that the application is starting in watch mode
 echo "starting application in watch mode..."
 
 # Start the continious build listener process
 echo "starting continuous build listener..."
-gradle build --continuous 2>&1 | tee builder.log & CONTINUOUS_BUILD_PID=$!
+gradle build --continuous 2>&1 | tee ./logs/builder.log & CONTINUOUS_BUILD_PID=$!
 
 # Start server process once initial build finishes  
-( while ! grep -m1 'BUILD SUCCESSFUL' < builder.log; do
-    sleep 15
+( while ! grep -m1 'BUILD SUCCESSFUL' < ./logs/builder.log; do
+    sleep 1
 done
 echo "starting crd server..."
-gradle bootRun 2>&1 | tee runner.log ) & SERVER_PID=$!
+gradle bootRun 2>&1 | tee ./logs/runner.log ) & SERVER_PID=$!
 
 # Handle application background process exiting
 wait $CONTINUOUS_BUILD_PID $SERVER_PID
